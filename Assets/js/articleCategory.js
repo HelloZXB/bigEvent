4bdd9a28744c9d59ac7b3674977a8e61839f5be0
HelloZXB <Hello16369@163.com>
2020-09-25 21:43:35 +0800

    文章分类页中的弹出层布局完成

--- Assets/js/articleCategory.js
+++ Assets/js/articleCategory.js
@@ -4,9 +4,17 @@ $(function () {
         url: '/my/article/cates',
         success: function (res) {
             if (res.status === 0) {
-                let htmlStr = template('.categoryList', res);
-                $('body').html(htmlStr);
+                let htmlStr = template('categoryList', res)
+                $('tbody').html(htmlStr);
             }
         }
+    })
+    $('.layui-card-header .btn-add').on('click', function () {
+        window.addIndex = layer.open({
+            type: 1, // 去掉右下角的确定按钮
+            title: '添加文章分类', // 添加弹出层的标题
+            area: '520px', // 设置弹出层宽高
+            content: $('#addCteTmp').html()
+        })
     })
 })
