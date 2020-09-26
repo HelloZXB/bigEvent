7f25037335b5e6f87cf932ca533a1ab1b2441ba6
HelloZXB <Hello16369@163.com>
2020-09-26 09:04:37 +0800

    添加文章分类完成

--- Assets/js/articleCategory.js
+++ Assets/js/articleCategory.js
@@ -1,14 +1,19 @@
 $(function () {
-    $.ajax({
-        type: 'get',
-        url: '/my/article/cates',
-        success: function (res) {
-            if (res.status === 0) {
-                let htmlStr = template('categoryList', res)
-                $('tbody').html(htmlStr);
+    renderTable();
+    function renderTable(){
+        $.ajax({
+            type: 'get',
+            url: '/my/article/cates',
+            success: function (res) {
+                if (res.status === 0) {
+                    let htmlStr = template('categoryList', res)
+                    $('tbody').html(htmlStr);
+                }
             }
-        }
-    })
+        })
+    }
+
+    // 弹出添加数据时的弹出层
     $('.layui-card-header .btn-add').on('click', function () {
         window.addIndex = layer.open({
             type: 1, // 去掉右下角的确定按钮
@@ -17,6 +22,8 @@ $(function () {
             content: $('#addCteTmp').html()
         })
     })
+
+    // 数据校验
     let form = layui.form;
     form.verify({
         username: function (value, item) { //value：表单的值、item：表单的DOM对象
@@ -30,5 +37,18 @@ $(function () {
                 return '用户名不能全为数字';
             }
         }
+    })
+
+    // 添加文章分类
+    $('body').on('submit', '.addForm', function (e) { // 事件委托
+        e.preventDefault(); // 阻止默认行为
+        $.ajax({
+            type: 'post',
+            url: '/my/article/addcates',
+            success: function (res) {
+                layer.close(window.addIndex); // 关闭弹出层
+                renderTable();
+            }
+        })
     })
 })
